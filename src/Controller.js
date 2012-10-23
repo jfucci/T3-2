ec9d75e3dfea373284688b781cbd9dc6247c77ef
jfucci <jacob_fucci@buacademy.org>
2012-10-23 19:43:22 -0400

    made model and view scoped to 'T3.Controller' and made the code more conscise

--- src/Controller.js
+++ src/Controller.js
@@ -1,17 +1,17 @@
 /*global $:true, document:true, T3:true */
 
 T3.Controller = function() {
-	model = new T3.Model();     //was var model
-	view = new T3.View(model);  //was var view
-	model.restart();
-	view.update();
+
+	var model = new T3.Model();     //was var model
+    var view = new T3.View(model);  //was var view
+
+	$("#restart").click(function(event){
+     model.restart();
+     view.update();
+   }).click();
 };
 
 //this function is called once the page is done loading
 $(document).ready(function() {
 	new T3.Controller();
-	$("a").click(function(event){
-     model.restart();
-     view.update();
-   });
 });
