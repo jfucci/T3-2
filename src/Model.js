45392750c6840b5e5a7af14311167028e8ee0a4c
jfucci <jacob_fucci@buacademy.org>
2012-10-24 12:37:20 -0400

    made checking for the winner more extensible

--- src/Model.js
+++ src/Model.js
@@ -7,7 +7,7 @@ T3.Model = function() {
 	this.currentPlayer = {};
 	this.winner = null;
 
-	this.board = new Array;
+	this.board = [];
 	for(var x = 0; x <= this.xCells; x++) {
 		this.board[x] = []; 
 		for(var y = 0; y <= this.xCells; y++) {
@@ -23,8 +23,8 @@ T3.Player = function(name) {
 T3.Model.prototype.restart = function() {
 	this.winner = null;
 	this.currentPlayer = this.players[Math.floor(Math.random() * 2)]; 
-	for(var x = 0; x <= this.xCells; x++) {
-		for(var y = 0; y <= this.xCells; y++) {
+	for(var x = 0; x < this.xCells; x++) {
+		for(var y = 0; y < this.yCells; y++) {
 			this.board[x][y] = null;
 		}
 	}
@@ -35,7 +35,7 @@ T3.Model.prototype.move = function(pixelX, pixelY) {
 	var widthInPixels = canvas.width();
 	var heightInPixels = canvas.height();
 	var cellWidthInPixels = widthInPixels / this.xCells;
-	var cellHeightInPixels = heightInPixels / this.xCells;
+	var cellHeightInPixels = heightInPixels / this.yCells;
 
 	var x = Math.floor(pixelX / cellWidthInPixels);	//find the x index of the cell
 	var y = Math.floor(pixelY / cellHeightInPixels); //find the y index of the cell
@@ -46,36 +46,91 @@ T3.Model.prototype.move = function(pixelX, pixelY) {
 	}
 };
 
-T3.Model.prototype.getWinner = function() {
-	var winner = null;
-
-	//check for a winner in the columns:
+T3.Model.prototype.checkRow = function(y) {
+	var count = 1;
 	for(var x = 0; x < this.xCells; x++) {
-		if(this.board[x][1]) {
-			if(this.board[x][1] === this.board[x][0] && this.board[x][1] === this.board[x][2]) {
-				winner = this.board[x][1];
-			}
+		if(this.board[x][y] && this.board[x][y] === this.board[x+1][y]) {
+			count++;
 		}
 	}
+	if(count === this.xCells) {
+		return true;
+	}
+}
 
-	//check for a winner in the rows:
+T3.Model.prototype.checkColumn = function(x) {
+	var count = 1;
 	for(var y = 0; y < this.xCells; y++) {
-		if(this.board[1][y]) {
-			if(this.board[1][y] === this.board[0][y] && this.board[1][y] === this.board[2][y]) {
-				winner = this.board[1][y];
-			}
+		if(this.board[x][y] && this.board[x][y] === this.board[x][y+1]) {
+			count++;
+		}
+	}
+	if(count === this.yCells) {
+		return true;
+	}
+}
+
+T3.Model.prototype.checkLeftDiagonal = function(x, y) {
+	var count = 1;
+
+	var xx;
+	var yy;
+	if(x > y) {
+		xx = x - y;
+		yy = y - y;
+	}
+	else{
+		xx = x - x;
+		yy = y - x;
+	}
+
+	for(; xx < this.xCells, yy < this.yCells; xx++, yy++) {
+		if(this.board[xx][yy] && this.board[xx][yy] === this.board[xx+1][yy+1]) {
+			count++;
 		}
 	}
 
-	//check for a winner in the diagonals:
-	if(this.board[1][1]) {
-		if(this.board[1][1] === this.board[0][0] && this.board[1][1] === this.board[2][2]) {
-			winner = this.board[1][1];
-		}
-		else if(this.board[1][1] === this.board[0][2] && this.board[1][1] === this.board[2][0]) {
-			winner = this.board[1][1];
-		}
+	if(count === this.xCells) {
+		return true;
+	}
+}
+
+T3.Model.prototype.checkRightDiagonal = function(x, y) {
+	var count = 1;
+
+	total = x + y;
+
+	if(total < (this.xCells - 1)) {
+		yy = 0;
+		xx = total;
+	}
+	else {
+		yy = total - (this.yCells - 1);
+		xx = total - yy;
 	}
 
+
+	while(xx > 0 && yy < (this.yCells - 1)) {
+		if(this.board[xx][yy] && this.board[xx][yy] === this.board[xx-1][yy+1]) {
+			count++;
+		}
+		xx--;
+		yy++;
+	}
+		
+	if(count === this.xCells) {
+		return true;
+	}
+
+}
+
+T3.Model.prototype.getWinner = function(player) {
+	var winner = null;
+	for(var x = 0, y = 0; x < this.xCells, y < this.yCells; x++, y++) {
+		if(this.checkRow(y) || this.checkColumn(x) || this.checkLeftDiagonal(x, y) || this.checkRightDiagonal(x, y)) {
+			winner = this.board[x][y];
+			break;
+		}
+	}
 	return winner;
-};
+}
