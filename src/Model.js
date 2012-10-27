4a94ad3d93d9ec4e7922294c8960db1a71dca700
jfucci <jacob_fucci@buacademy.org>
2012-10-27 15:54:44 -0400

    Added a connect four mode! Why? Because I could.

--- src/Model.js
+++ src/Model.js
@@ -1,17 +1,32 @@
 /*global _:true, T3:true */
 
 T3.Model = function() {
-	this.xCells = 3;
-	this.yCells = 3;
-	this.winnerSize = 3;
 	this.players = [new T3.Player('X'), new T3.Player('O')];
 	this.currentPlayer = {};
 	this.winner = null;
 
+	//make the user choose tic tac toe or connect four, and set the variables accordingly:
+	var mode = 0;
+	while(mode !== '1' && mode !== '2') {
+		mode = prompt("Tic Tac Toe (1) or Connect Four (2)?");
+	}
+	if(mode === '1') {
+		this.xCells = 3;
+		this.yCells = 3;
+		this.winnerSize = 3;
+		this.gravity = 0;
+	}
+	else {
+		this.xCells = 10;
+		this.yCells = 10;
+		this.winnerSize = 4;
+		this.gravity = 1;
+	}
+
 	this.board = [];
 	for(var x = 0; x <= this.xCells; x++) {
 		this.board[x] = []; 
-		for(var y = 0; y <= this.xCells; y++) {
+		for(var y = 0; y <= this.yCells; y++) {
 			this.board[x][y] = null;
 		}
 	}
@@ -42,6 +57,11 @@ T3.Model.prototype.move = function(pixelX, pixelY) {
 	var y = Math.floor(pixelY / cellHeightInPixels); //find the y index of the cell
 
 	if(!(this.board[x][y])) {
+		if(this.gravity) {
+			for(y = this.yCells -1; y >= 0; y--) {
+				if(!(this.board[x][y])) { break; }
+			}
+		}
 		this.board[x][y] = this.currentPlayer.name;
 		this.currentPlayer = this.players[(this.players.indexOf(this.currentPlayer) + 1) % this.players.length];
 	}
@@ -51,13 +71,17 @@ T3.Model.prototype.checkRow = function(x, y) {
 	var count = 1;
 
 	for(var xx = x; xx > 0 && xx >= x - this.winnerSize; xx--) {
-		if(this.board[xx][y] === this.board[xx-1][y] && this.board[xx][y] === this.board[x][y]) {
+		if(this.board[xx][y] === this.board[xx-1][y] && 
+			this.board[xx][y] === this.board[x][y]) 
+		{
 			count++;
 		}
 	}
 	
 	for(xx = x; xx < this.xCells && xx <= x + this.winnerSize; xx++) {
-		if(this.board[xx][y] === this.board[xx+1][y] && this.board[xx][y] === this.board[x][y]) {
+		if(this.board[xx][y] === this.board[xx+1][y] && 
+			this.board[xx][y] === this.board[x][y]) 
+		{
 			count++;
 		}
 	}
@@ -69,13 +93,17 @@ T3.Model.prototype.checkRow = function(x, y) {
 T3.Model.prototype.checkColumn = function(x, y) {
 	var count = 1;
 	for(var yy = y; yy > 0 && yy >= y - this.winnerSize; yy--) {
-		if(this.board[x][yy] === this.board[x][yy-1] && this.board[x][yy] === this.board[x][y]) {
+		if(this.board[x][yy] === this.board[x][yy-1] && 
+			this.board[x][yy] === this.board[x][y]) 
+		{
 			count++;
 		}
 	}
 
 	for(yy = y; yy < this.yCells && yy <= y + this.winnerSize; yy++) {
-		if(this.board[x][yy] === this.board[x][yy+1] && this.board[x][yy] === this.board[x][y]) {
+		if(this.board[x][yy] === this.board[x][yy+1] && 
+			this.board[x][yy] === this.board[x][y]) 
+		{
 			count++;
 		}
 	}
@@ -127,7 +155,6 @@ T3.Model.prototype.checkRightDiagonal = function(x, y) {
 	while (xx < this.xCells && xx <= x + this.winnerSize && 
 		yy > 0 && yy >= y - this.winnerSize) 
 	{
-
 		if(this.board[xx][yy] === this.board[xx+1][yy-1] && 
 			this.board[xx][yy] === this.board[x][y]) 
 		{
@@ -141,7 +168,6 @@ T3.Model.prototype.checkRightDiagonal = function(x, y) {
 	while (xx > 0 && xx >= x - this.winnerSize && 
 		yy < this.yCells && yy <= y + this.winnerSize) 
 	{
-
 		if(this.board[xx][yy] === this.board[xx-1][yy+1] && 
 			this.board[xx][yy] === this.board[x][y]) 
 		{
